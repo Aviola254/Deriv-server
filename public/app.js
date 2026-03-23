f6ecfadfdbf85749ab96a8171bb21a0c6e49f984
Aviola254 <aviolaavidar307@gmail.com>
2026-03-24 01:17:02 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,35 +1,45 @@
-const token = "fCS24qkOpJQCM7p";
-
-const app_id = 1089;
+const token = "PUT_TOKEN";
+let symbol = "R_75";
 
 const ws = new WebSocket(
-  "wss://ws.derivws.com/websockets/v3?app_id=" + app_id
+  "wss://ws.derivws.com/websockets/v3?app_id=1089"
 );
 
-ws.onopen = function () {
+ws.onopen = () => {
 
   ws.send(JSON.stringify({
     authorize: token
   }));
 
-  ws.send(JSON.stringify({
-    ticks: "R_75",
-    subscribe: 1
-  }));
+  subscribe();
 
 };
 
-ws.onmessage = function (msg) {
+function subscribe() {
+
+  ws.send(JSON.stringify({
+    ticks: symbol,
+    subscribe: 1
+  }));
+
+}
+
+ws.onmessage = (msg) => {
 
   const data = JSON.parse(msg.data);
 
   if (data.tick) {
 
-    const price = data.tick.quote;
-
     document.getElementById("chart").innerText =
-      "Vol75 Price: " + price;
+      symbol + " : " + data.tick.quote;
 
   }
+
+};
+
+document.getElementById("market").onchange = (e) => {
+
+  symbol = e.target.value;
+  subscribe();
 
 };
