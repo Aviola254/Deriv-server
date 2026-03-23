989558333957c718cb8dd6af52160097dd5913b2
Aviola254 <aviolaavidar307@gmail.com>
2026-03-24 01:04:57 +0300

    Update app.js

--- public/app.js
+++ public/app.js
@@ -1,9 +1,35 @@
-console.log("app ready")
+const token = "fCS24qkOpJQCM7p";
 
-function startBot(){
-  console.log("bot start")
-}
+const app_id = 1089;
 
-function stopBot(){
-  console.log("bot stop")
-}
+const ws = new WebSocket(
+  "wss://ws.derivws.com/websockets/v3?app_id=" + app_id
+);
+
+ws.onopen = function () {
+
+  ws.send(JSON.stringify({
+    authorize: token
+  }));
+
+  ws.send(JSON.stringify({
+    ticks: "R_75",
+    subscribe: 1
+  }));
+
+};
+
+ws.onmessage = function (msg) {
+
+  const data = JSON.parse(msg.data);
+
+  if (data.tick) {
+
+    const price = data.tick.quote;
+
+    document.getElementById("chart").innerText =
+      "Vol75 Price: " + price;
+
+  }
+
+};
